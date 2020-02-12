fe714acc844cf0dc500fbe8ee5f72b16054c7768
Carlos Azaustre <cazaustre@gmail.com>
2020-02-12 11:10:06 +0100

    post: api rest node

--- src/components/youtube.jsx
+++ src/components/youtube.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default ({ videoId }) => {
+export default ({ videoId, playlist }) => {
   return (
     <div
       className="video"
@@ -11,18 +11,35 @@ export default ({ videoId }) => {
         height: 0
       }}
     >
-      <iframe
-        title={videoId}
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%"
-        }}
-        src={`https://www.youtube.com/embed/${videoId}`}
-        frameBorder="0"
-      />
+      {videoId && (
+        <iframe
+          title={videoId}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%"
+          }}
+          src={`https://www.youtube.com/embed/${videoId}`}
+          frameBorder="0"
+        />
+      )}
+
+      {playlist && (
+        <iframe
+          title={videoId}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%"
+          }}
+          src={`https://www.youtube.com/embed/videoseries?list=${playlist}`}
+          frameBorder="0"
+        />
+      )}
     </div>
   );
 };
