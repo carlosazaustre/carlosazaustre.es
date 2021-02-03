e80b2ad798b18780f4474d7187bd070a3a136b96
Carlos Azaustre <cazaustre@gmail.com>
2021-02-03 09:08:06 +0100

    bug fix on input

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -98,7 +98,7 @@ export default ({
       </div>
       <input type="hidden" name="ml-submit" value="1" />
       <div className="ml-form-embedSubmit">
-        <input type="submit" value="Subscribe" name="member[subscribe]" id="member_submit">
+        <input type="submit" value="Subscribe" name="member[subscribe]" id="member_submit" />
         <button
           disabled="disabled"
           style={{ display: "none" }}
@@ -114,7 +114,10 @@ export default ({
           </div>{" "}
         </button>
       </div>
-          <div class="revue-form-footer">By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
+          <div class="revue-form-footer">
+              By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms</a> 
+              and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.
+        </div>
     </form>
     <small style={{ display: "block", color: "#222", textAlign: "center", margin: "0 auto" }}>
       <span role="img" aria-label="secure">
